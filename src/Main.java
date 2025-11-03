db72d7c9d62c18df8a90109de5612ae359df9961
Wesley Amaral de Carvalho <0001155125@senaimgaluno.com.br>
2025-11-03 10:53:29 -0300

    Teste

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Clonando e alterando o projeto da dupla...");
         System.out.println("Tudo correto!");
+        System.out.println("Teste");
 
     }
 }
